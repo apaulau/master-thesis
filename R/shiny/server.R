1200edd7d57a2371eb72b1f23b8cd2aa1597babd
Aliaksandr Paulau <apaulau@searshc.com>
2015-04-07 15:30:54 +0300

    added a few pages to residual analysis tab

--- R/shiny/server.R
+++ R/shiny/server.R
@@ -54,6 +54,9 @@ shinyServer(function(input, output, session) {
   model <- reactive({
     lm(series()$temperature ~ c(minYear():maxYear()))
   })
+  residuals <- reactive({
+    data.frame(year = series()$year, temperature=model()$residuals)
+  })
   
   output$datasource <- renderDataTable({
     df <- series()
@@ -150,7 +153,7 @@ shinyServer(function(input, output, session) {
     if (input$residuals_trigger == "src") {
       data.frame(series(), line())
     } else {
-      data.frame(temperature=model()$residuals, year = series()$year, x_rng=breaks(), y_rng=rep(0, maxYear() - minYear() + 1))
+      data.frame(residuals(), x_rng=breaks(), y_rng=rep(0, maxYear() - minYear() + 1))
     }
   })
   
@@ -204,9 +207,66 @@ shinyServer(function(input, output, session) {
   })
   
   output$residual_source <- renderDataTable({
-    df <- data.frame(year = series()$year, sapply(X=model()$residuals, FUN=format, digits=4))
+    df <- residuals()
     colnames(df) <- c("Год наблюдения", "Температура")
     df
+  })
+  
+  residuals %>% ggvis(~year, ~temperature) %>% layer_points() %>% layer_paths() %>% 
+    add_axis("x", title="Год наблюдения", format="d", properties=axis_props(labels=list(angle=45, align="left"))) %>%
+    add_axis("y", title="Температура, ºС", title_offset=60) %>%
+    add_tooltip(function(df) paste(df$year, ":", df$temperature)) %>%
+    scale_numeric("x", nice = FALSE) %>%
+    bind_shiny("residual_overview", "overview_ui")
+  
+  residual_binwidth <- reactive({input$residual_binwidth})
+  
+  output$residual_plot <- renderPlot({
+    plot <- NA
+    
+    if (input$residual_base_plot_trigger == "histogram") {
+      width <- residual_binwidth()
+      
+      if (width > 0) {
+        plot <- DrawHistogram(data=residuals(), binwidth=width, fit=input$residual_dnorm)
+      } else {
+        plot <- DrawHistogram(data=residuals(), fit=input$residual_dnorm)
+      }
+      
+      if (input$residual_density) {
+        plot <- plot + geom_density(colour="#999999", fill="#009E73", alpha=.5)
+      } 
+    } else {
+      plot <- DrawQuantileQuantile(data=residuals()$temperature)
+    }
+    
+    plot
+  })
+  
+  output$residual_rule <- renderText({
+    rule <- switch(input$residual_rule,
+      sturges = sturges,
+      scott = scott,
+      fd = fd)
+    
+    format(rule(), digits=3)
+  })
+  
+  output$residual_dstats <- renderTable({
+    dstats.describe(residuals()$temperature, locale=TRUE)
+  })
+  
+  output$residual_normality <- renderUI({
+    test <- switch(input$residual_ntest,
+      shapiro = ntest.ShapiroWilk,
+      pearson = ntest.PearsonChi2,
+      ks = ntest.KolmogorovSmirnov)
+    print(residuals()$temperature)
+    result <- test(data=residuals()$temperature)
+    statistic <- paste("<b>Статистика:</b>", format(result$statistic[[1]]))
+    p.value <- paste("<b>P-значение:</b>", format(result$p.value))
+    conclusion <- paste("<b>Заключение:</b>", ifelse(result$p.value <= .05, "Нулевая гипотеза отклонена.", "Нельзя отвергнуть нулевую гипотезу."))
+    HTML(paste(statistic, p.value, conclusion, sep = '<br/>'))
   })
   
 })
